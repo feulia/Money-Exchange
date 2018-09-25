a23133a790bd59fe9f933a862dd4e7d7d23bb903
Yulia <43168694+feulia@users.noreply.github.com>
2018-09-25 18:53:20 +0300

    Rewrite the method

--- src/index.js
+++ src/index.js
@@ -1,63 +1,57 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
   var types = {
-    p: 0,
-    n: 5,
     d: 10,
-    q: 25,
-    h: 50
+    h: 50,
+    n: 5,
+    p: 0,
+    q: 25
   };
-  var result = {
-    P: 0,
-    N: 0,
-    D: 0,
-    Q: 0,
-    H: 0
-  };
+  var result = {};
   var maxNum = 10000;
 
-  function isType(type) {
-    return (1 <= currency / type);
+  function isType(currencyType) {
+    return (1 <= currency / currencyType);
   }
 
-  function getAmount(type) {
-    return Math.floor(currency / type);
+  function getAmount(currencyType) {
+    return Math.floor(currency / currencyType);
   }
 
-  function decreaseCurrencyBy(type) {
-    return currency % type;
+  function decreaseCurrencyBy(currencyType) {
+    return currency % currencyType;
   }
 
   if (currency > maxNum) {
-    
-    return  { error: "You are rich, my friend! We don't have so much coins for exchange" };
-  
+
+    return  { error: "You are rich, my friend! We don\'t have so much coins for exchange" };
+
   } else {
-
-    if (isType(types.p)) {
-      result.P = currency;
-    }
-
-    if (isType(types.n)) {
-      result.N = getAmount(types.n);
-      currency = decreaseCurrencyBy(types.n);
-    }
-
-    if (isType(types.d)) {
-      result.D = getAmount(types.d);
-      currency = decreaseCurrencyBy(types.d);
-    }
-
-    if (isType(types.q)) {
-      result.Q = getAmount(types.q);
-      currency = decreaseCurrencyBy(types.q);
-    }
 
     if (isType(types.h)) {
       result.H = getAmount(types.h);
       currency = decreaseCurrencyBy(types.h);
     }
     
+    if (isType(types.q)) {
+      result.Q = getAmount(types.q);
+      currency = decreaseCurrencyBy(types.q);
+    }
+    
+    if (isType(types.d)) {
+      result.D = getAmount(types.d);
+      currency = decreaseCurrencyBy(types.d);
+    }
+
+    if (isType(types.n)) {
+      result.N = getAmount(types.n);
+      currency = decreaseCurrencyBy(types.n);
+    }
+    
+    if (isType(types.p)) {
+      result.P = currency;
+    }
+      
     return result;
   }
   // Return an object containing the minimum number of coins needed to make change
